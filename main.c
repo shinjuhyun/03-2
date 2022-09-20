6e086c60e05a5617f2c0d27708e2d6cf0514d472
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-09-20 17:37:16 +0900

    01

--- main.c
+++ main.c
@@ -3,7 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(void){
+	int input_int;
+	float input_float;
 	
-	return 0;
+	printf("enter an integer : ");
+	scanf("%d" ,&input_int);
+	
+	printf("enter a float : ");
+	scanf("%f",&input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+		return 0;
 }
